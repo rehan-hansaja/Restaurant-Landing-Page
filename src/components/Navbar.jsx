aa055dca6fb8b5f98d6003e403e2c6b465021095
rehan-hansaja <rehanhansaja01@gmail.com>
2024-02-13 16:24:09 +0530

    UI design code uploaded.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import Logo from './assets/Logo.svg'
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+
+
+export const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    const menuOptions =  [
+        {
+            text: "Home",
+            icon: <HomeIcon />,
+            path: '/',
+          },
+          {
+            text: "About",
+            icon: <InfoIcon />,
+            path: '/about',
+          },
+          {
+            text: "Testimonials",
+            icon: <CommentRoundedIcon />,
+            path: '/textimonials',
+          },
+          {
+            text: "Contact",
+            icon: <PhoneRoundedIcon />,
+            path: '/contact',
+          },
+          {
+            text: "Cart",
+            icon: <ShoppingCartRoundedIcon />,
+            path: '/cart',
+          },
+        ];
+    
+
+
+
+
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Testimonials</a>
+        <a href="">Contact</a>
+        <a href="">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+          
+        
+        <button className="primary-button">Bookings Now</button>
+      </div>
+
+      {/* <div className="navbar-links-container">
+        {menuOptions.map((item) => (
+          <Link to={item.path} key={item.text}>
+            {item.text}
+          </Link>
+        ))}
+        <BsCart2 className="navbar-cart-icon" />
+        <button className="primary-button">Bookings Now</button>
+      </div> */}
+
+
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
+    </nav>
+    
+  )
+}
+
