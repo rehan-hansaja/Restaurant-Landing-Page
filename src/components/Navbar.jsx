6912ff1e4a665988f98b873aa306593f8239319b
rehan-hansaja <rehanhansaja01@gmail.com>
2024-02-17 14:56:53 +0530

    changed logo & colours

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import Logo from './assets/Logo.svg'
+import Logo from './assets/Logo.png'
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import Box from "@mui/material/Box";
