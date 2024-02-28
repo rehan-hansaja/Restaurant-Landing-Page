35b60ee8bf5b79d46e3784a84f575c1cf9693a3d
rehan-hansaja <rehanhansaja01@gmail.com>
2024-02-28 19:40:08 +0530

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProfilePic from "./assets/john-doe-image.png";
-import { AiFillStar } from "react-icons/ai";
+import { FillStar } from "react-icons/ai";
 
 export const Testimonials = () => {
   return (
@@ -9,24 +9,24 @@ export const Testimonials = () => {
         <p className="primary-subheading">Testimonial</p>
         <h1 className="primary-heading">What They Are Saying</h1>
         <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+          Prepare to be dazzled by the culinary delights and hospitality that await you. 
+          It's more than just a restaurant; it's an extraordinary culinary journey you won't soon forget.
         </p>
       </div>
       <div className="testimonial-section-bottom">
         <img src={ProfilePic} alt="" />
         <p>
-          Sri lanka is one of the fast jkfj uhei heidj heidj
-          hefwjekowk ehfwjhe wjeifjwie ijeifjwe 
+          Each dish served was a masterpiece, meticulously crafted with the freshest ingredients and 
+          bursting with flavors that danced on my palate. The chef's dedication to excellence truly shone through in every bite.
         </p>
         <div className="testimonials-stars-container">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
+          <FillStar />
+          <FillStar />
+          <FillStar />
+          <FillStar />
+          <FillStar />
         </div>
-        <h2>John Doe</h2>
+        <h2>James Anderson</h2>
       </div>
     </div>
   );
