39deed016450a1c993eaff438957d55dc6a259c9
rehan-hansaja <rehanhansaja01@gmail.com>
2024-02-28 22:37:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { About } from './components/About';
 import { Home } from './components/Home';
 import { Work } from './components/Work';
 import { Footer } from './components/Footer';
 import { Testimonials } from './components/Testimonials';
 import { Contact } from './components/Contact';
+
 
 function App() {
   return (
@@ -16,23 +17,20 @@ function App() {
       <Testimonials/>
       <Contact/>
       <Footer/>
-      {/* <Router/> */}
-
-      {/* <Router>
-      <div className="App">
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router> */}
-
     </div>
+
+      
+      // <div className="App">
+      //   <Routes>
+      //     <Route path="/" element={<Home />} />
+      //     <Route path="/About" element={<About />} />
+      //     <Route path="/Work" element={<Work />} />
+      //     <Route path="/Testimonials" element={<Testimonials />} />
+      //     <Route path="/Contact" element={<Contact />} />
+      //     <Route path="/Footer" element={<Footer />} />
+      //   </Routes>
+      // </div>
+
   );
 }
 
